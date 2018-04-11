3318ee45ea1b43d6d85d5c2f8191289037d570fc
Daniela Zepeda <zepeda218@hotmail.com>
2018-04-11 12:34:57 -0600

    Js changes

---
+++ CampusVirtual/CampusVirtual/wwwroot/js/FormMaterias.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    $('form').submit(function (event) {
+        var mensaje = "";
+        var mensajeN = "";
+        $('.validate').each(function () {
+            var label = $('label', this.parentElement).text();
+            var value = $(this).val();
+
+            if (($(this).val().trim().length < 5) && (label.substring(0, label.length - 1) != "Precio")
+                && (label.substring(0, label.length - 1) != "Créditos")) {
+                var label = $('label', this.parentElement).text();
+                mensaje += "\n" + label.substring(0, label.length - 1);
+            }
+
+            if (value < 0) {
+                var label = $('label', this.parentElement).text();
+                mensajeN += "\n" + label.substring(0, label.length - 1);
+            }
+        });
+        if (mensaje != "") {
+            alert("Los siguiente campos tienen menos de 5 caracteres:\n" + mensaje);
+            event.preventDefault(event);
+        }
+        if (mensajeN != "") {
+            alert("Los siguientes campos no puede ser menor a 0:\n" + mensajeN);
+            event.preventDefault(event);
+        }
+    });
+})();
+
+jQuery('.no-espacios').keyup(function (e) {
+    var value = jQuery(this).val();
+    if (value == " ") {
+        jQuery(this).val("");
+    }
+});
+
+function myFunctionDelete() {
+    alert("Se borrará la materia");
+}
+
+function myFunctionDeleteReq() {
+    alert("Se borrará el requisito");
+}
---
+++ CampusVirtual/CampusVirtual/wwwroot/js/Usuario.js
@@ -0,0 +1,50 @@
+﻿//Datatables
+jQuery(document).ready(function () {
+    jQuery('.dataTable').DataTable({
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
+        },
+        responsive: true
+    });
+
+//No numbers
+    jQuery('.not-number').keyup(function (e) {
+        if (!/^[ a-záéíóúüñ]*$/i.test(this.value)) {
+            this.value = this.value.replace(/[^ a-záéíóúüñ]+/ig, "");
+        }
+    });
+
+//Id validation
+    jQuery('#identificacion').change(function (e) {
+        if (this.value == "Nacional") {
+            jQuery('#cedula').attr("min", "100000000");
+            jQuery('#cedula').attr("max", "999999999");
+            jQuery('#cedula').attr("type", "number");
+        } else {
+            jQuery('#cedula').removeAttr("min");
+            jQuery('#cedula').removeAttr("max");
+            jQuery('#cedula').attr("type", "text");
+        }
+    });
+
+ //No spaces
+    jQuery('.no-espacios').keyup(function (e) {
+        var value = jQuery(this).val();
+        if (value == " ") {
+            jQuery(this).val("");
+        }
+    });
+
+//?
+    jQuery(".editar-usuario").click(function () {
+        jQuery(".titulo-form-usuario").html("Editar usuario");
+    });
+    jQuery(".nuevo-usuario").click(function () {
+        jQuery(".titulo-form-usuario").html("Nuevo usuario");
+    });
+});
+
+function myFunctionDelete() {
+    alert("El usuario se borrará.");
+}	
+});
