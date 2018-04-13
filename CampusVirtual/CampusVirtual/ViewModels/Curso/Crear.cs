a34fe34a1cae8ebd2622f6c8528ec7bddb2b4427
jose zuniga <jsezuniga72@gmail.com>
2018-04-13 12:45:16 -0600

    Cursos-Asistencia

---
+++ CampusVirtual/CampusVirtual/ViewModels/Curso/Crear.cs
@@ -0,0 +1,23 @@
+﻿using CampusVirtual.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampusVirtual.ViewModels.Curso
+{
+    public class Crear
+    {
+
+		[Required(ErrorMessage = "El nombre del curso es requerido.")]
+		[Display(Name = "Nombre del Curso")]
+		public string Nombre { get; set; }
+		[Display(Name = "UCs")]
+		public ICollection<UsuarioCurso> UCs { get; set; }
+		[Display(Name = "Evaluaciones")]
+		public ICollection<Evaluacion> Evaluaciones { get; set; }
+		public string Title { get; set; }
+		public bool ExisteCurso { get; set; }
+	}
+}
---
+++ CampusVirtual/CampusVirtual/ViewModels/Curso/Editar.cs
@@ -0,0 +1,18 @@
+﻿using CampusVirtual.Model.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampusVirtual.ViewModels.Curso
+{
+    public class Editar
+    {
+	
+		public string Nombre { get; set; }
+		public ICollection<UsuarioCurso> UCs { get; set; }
+		public ICollection<Evaluacion> Evaluaciones { get; set; }
+		public string Title { get; set; }
+	}
+}
